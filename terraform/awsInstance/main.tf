805b225fb6a40faf04a678a46d7e3ec38780ff4c
CHIMMILI <muralichimmili@gmail.com>
2021-11-25 20:41:00 +0530

    updated changes

--- terraform/awsInstance/main.tf
+++ terraform/awsInstance/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "sample" {
-  count         = len(var.name)
+  count         = length(var.name)
   ami           = "ami-0855cab4944392d0a"
   instance_type = var.instype
   vpc_security_group_ids = [var.secgrpid]
