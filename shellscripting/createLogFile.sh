af35bd5d4edaf2c19dff68e8a4b8195c57915e52
CHIMMILI <muralichimmili@gmail.com>
2021-11-26 14:40:46 +0530

    updated changes

--- shellscripting/createLogFile.sh
+++ shellscripting/createLogFile.sh
@@ -2,8 +2,8 @@
 
   timestamp=$(date +%d_%m_%Y_%H_%M_%S)
 
-  echo "this data is to log file" > ${timestamp}.log
+  echo this data is to log file > ${timestamp}.log
 
-  echo "this is extra data to loig file" >> ${timestamp}.log
+  echo this is extra data to log file >> ${timestamp}.log
 
   date >> ${timestamp}.log
