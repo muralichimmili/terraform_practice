5e58a4ef955ce60aa9bdeae15e8947527cd30dc5
CHIMMILI <muralichimmili@gmail.com>
2021-11-26 20:40:55 +0530

    updated changes

--- shellscripting/createLogFile.sh
+++ shellscripting/createLogFile.sh
@@ -1,6 +1,6 @@
 #! bin/bash
 
   timestamp=$(date +%d_%m_%Y_%H_%M_%S)
-  echo "this is test data to text file" >> ./home/devops/sample.log
-  echo "this is extra data to log file" >> ./home/devops/sample.log
-  date >> ./home/devops/sample.log
+  echo "this is test data to text file" >> $timestamp.log
+  echo "this is extra data to log file" >> $timestamp.log
+  date >> $timestamp.log
