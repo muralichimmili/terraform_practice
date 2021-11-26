3a87b792fa752f43aafca19f697f5cad4dfb640a
CHIMMILI <muralichimmili@gmail.com>
2021-11-26 12:24:44 +0530

    updated changes

--- shellscripting/variable_sample.sh
+++ shellscripting/variable_sample.sh
@@ -2,8 +2,8 @@
 
 A=10
 B=20
-USER_NAME = "murali"
-USER_ACTION = "sleep"
+USER_NAME =murali
+USER_ACTION =sleep
 echo "values for a and b are : $A $B"
 echo "username is : $USER_NAME"
 echo "user is : $USER_ACTION"
