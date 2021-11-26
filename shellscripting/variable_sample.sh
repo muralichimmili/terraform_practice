5fa18945830289b9ee638cd23cc700b1e8c02a29
CHIMMILI <muralichimmili@gmail.com>
2021-11-26 12:43:39 +0530

    updated changes

--- shellscripting/variable_sample.sh
+++ shellscripting/variable_sample.sh
@@ -3,8 +3,8 @@
 a=10
 b=20
 COURSE="linux"
-ACTION="SLEEP"
+FAVACT="SLEEP"
 echo "Values of a and b are: $a and $b"
 echo "My Course is: ${COURSE}"
-echo "My Favourite Action: $ACTIONING"
-echo "My Favourite Action: ${ACTION}ING"
+echo "My Favourite Action: $FAVACTING"
+echo "My Favourite Action: ${FAVACT}ING"
