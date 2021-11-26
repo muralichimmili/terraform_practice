eae17399ef77d0f5cef2aa50bf314255346bb2d2
CHIMMILI <muralichimmili@gmail.com>
2021-11-26 12:41:09 +0530

    updated changes

--- shellscripting/variable_sample.sh
+++ shellscripting/variable_sample.sh
@@ -3,8 +3,8 @@
 a=10
 b=20
 COURSE="linux"
-ACTION="SLEEP" e
-cho "Values of a and b are: $a and $b"
+ACTION="SLEEP"
+echo "Values of a and b are: $a and $b"
 echo "My Course is: ${COURSE}"
 echo "My Favourite Action: $ACTIONING"
 echo "My Favourite Action: ${ACTION}ING"
