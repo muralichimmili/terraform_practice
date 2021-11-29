59964ab591e2c4f8117d0c1d5549efc5ab2c89ec
CHIMMILI <muralichimmili@gmail.com>
2021-11-29 11:01:46 +0530

    updated changes

--- shellscripting/CreateLogFileDaily.sh
+++ shellscripting/CreateLogFileDaily.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
 dir=$(date +%d_%m_%Y)
 file=$(date +%d_%m_%Y_%H_%M_%S)
- if [!(-e $dir)];then
+ if [ ! -e $dir ]; then
     mkdir $dir
     echo "Created Directory successfully"
  else
    echo "$dir already exist in the system"
  fi
 
-  if[!(-e ${file}.log)];then
+  if[ ! -e ${file}.log ]; then
        touch ${file}.log
        echo "${file}.log created successfully"
      else
