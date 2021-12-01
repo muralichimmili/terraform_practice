d4b74869eeaf06808dd017f7408d342ceb2d9bd9
CHIMMILI <muralichimmili@gmail.com>
2021-12-01 20:17:07 +0530

    updated changes

--- httpserverinstance/main.tf
+++ httpserverinstance/main.tf
@@ -69,6 +69,14 @@ resource "aws_security_group" "allow_sample" {
     ipv6_cidr_blocks = ["::/0"]
   }
 
+  egress {
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
   tags = {
     Name = "webserver sg"
   }
