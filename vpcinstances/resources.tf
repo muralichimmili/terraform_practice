7f12bc973b32f1c55deb4487f7f2ccd4ea6528ce
CHIMMILI <muralichimmili@gmail.com>
2021-12-01 18:09:55 +0530

    updated changes

--- vpcinstances/resources.tf
+++ vpcinstances/resources.tf
@@ -1,12 +1,12 @@
-resource "aws_key_pair" "default" {
+/*resource "aws_key_pair" "default" {
   key_name = "vpckeypair"
   public_key = "${file("${var.key_path}")}"
-}
+}*/
 
 resource "aws_instance" "wb" {
   ami = "${var.ami}"
   instance_type = "t2.micro"
-  key_name = "${aws_key_pair.default.id}"
+  #key_name = "${aws_key_pair.default.id}"
   subnet_id = "${aws_subnet.public-subnet.id}"
   vpc_security_group_ids = ["${aws_security_group.sgroupweb.id}"]
   associate_public_ip_address = true
@@ -21,7 +21,7 @@ resource "aws_instance" "wb" {
 resource "aws_instance" "db" {
   ami = "${var.ami}"
   instance_type = "t2.micro"
-  key_name = "${aws_key_pair.default.id}"
+ # key_name = "${aws_key_pair.default.id}"
   subnet_id = "${aws_subnet.private-subnet.id}"
   vpc_security_group_ids = ["${aws_security_group.sgroupdb.id}"]
   source_dest_check = false
